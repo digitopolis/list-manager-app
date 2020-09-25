cac761eeeb1e0298682324dc97f0d1a36ff1b8f0
Matt Readout <matt.readout@gmail.com>
2020-09-25 11:25:33 -0700

    Add validation, submit handler to signinForm

--- src/forms/signInForm.tsx
+++ src/forms/signInForm.tsx
@@ -1,42 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import "./form.css";
+import { validateEmail, validatePassword } from "./validators";
+import { LOGIN } from "../apiEndpoints";
+import { Redirect } from "react-router";
 
 interface Values {
   email: string;
   password: string;
 }
 
-const SignInForm: React.FC<{}> = () => (
-  <div className="signup-form">
-    <h1>Sign in!</h1>
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-      onSubmit={(values: Values) => {
-        alert(
-          `submitting! email: ${values.email}, password: ${values.password}`
-        );
-      }}
-    >
-      <Form className="form">
-        <label htmlFor="email">Email</label>
-        <Field id="email" name="email" placeholder="Email" />
+const SignInForm: React.FC<{}> = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleSubmit = (values: Values) => {
+    fetch(LOGIN, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(values),
+    })
+      .then((res) => res.json())
+      .then(handleResponse);
+  };
 
-        <label htmlFor="password">Password</label>
-        <Field
-          id="password"
-          name="password"
-          type="password"
-          placeholder="Password"
-        />
+  const handleResponse = (response: {
+    user?: { email: string };
+    error?: string;
+  }) => {
+    if (response.user) {
+      setLoggedIn(true);
+    } else if (response.error) {
+      setErrorMessage(response.error);
+    }
+  };
 
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
-  </div>
-);
+  return (
+    <div className="signup-form">
+      {loggedIn ? <Redirect to="/profile" /> : null}
+      <h1>Sign in!</h1>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched }) => (
+          <Form className="form">
+            <label htmlFor="email">Email</label>
+            <Field
+              id="email"
+              name="email"
+              placeholder="Email"
+              validate={validateEmail}
+            />
+            {errors.email && touched.email ? (
+              <div className="error-message">{errors.email}</div>
+            ) : null}
+
+            <label htmlFor="password">Password</label>
+            <Field
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              validate={validatePassword}
+            />
+            {errors.password && touched.password ? (
+              <div className="error-message">{errors.password}</div>
+            ) : null}
+
+            <button type="submit">Submit</button>
+            {errorMessage !== "" ? (
+              <div className="error-message">{errorMessage}</div>
+            ) : null}
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default SignInForm;
