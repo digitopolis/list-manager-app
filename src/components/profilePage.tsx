6b612f9e07a259120f415c308d43c456aa59575d
Matt Readout <matt.readout@gmail.com>
2020-10-01 12:25:03 -0700

    Clean up import statements

--- src/components/profilePage.tsx
+++ src/components/profilePage.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import "./components.css";
 import { User } from "../interfaces/user";
-import { List } from "../interfaces/list";
 import { ListContainer } from "../containers/listContainer";
-import { userInfo } from "os";
 
 const ProfilePage: React.FC<{ user: User | null }> = (props) => {
   const { user } = props;
