fc9581feb9baf80b4ac512bc417dfe2ac8a2c11d
Matt Readout <matt.readout@gmail.com>
2020-10-01 17:27:27 -0700

    Update profile with toggle button, setLists

--- src/components/profilePage.tsx
+++ src/components/profilePage.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./components.css";
 import { User } from "../interfaces/user";
+import { List } from "../interfaces/list";
 import ListContainer from "../containers/listContainer";
+import NewListForm from "../forms/newListForm";
 
 const ProfilePage: React.FC<{ user: User | null }> = (props) => {
   const { user } = props;
+  const [lists, setLists] = useState(user ? user.lists : []);
+  const [showNewListForm, toggleShowForm] = useState(false);
+
+  const addList = (list: List) => {
+    setLists([...lists, list]);
+  };
+
+  const showForm = (): JSX.Element => {
+    return (
+      <NewListForm
+        user_id={user ? user.id : 0}
+        addList={addList}
+        toggleShowForm={toggleShowForm}
+      />
+    );
+  };
+
+  const showButton = () => {
+    return <button onClick={() => toggleShowForm(true)}>Create List</button>;
+  };
+
   return (
     <div className="profile-page">
-      <ListContainer lists={user ? user.lists : []} />
+      {showNewListForm ? showForm() : showButton()}
+      <ListContainer lists={lists} />
     </div>
   );
 };
